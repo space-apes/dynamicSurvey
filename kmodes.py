b510f56e6a117adb854eb3e1e14c5e58a38575ed
Brian R. Smith <brianrsmith080@gmail.com>
2021-11-13 00:57:50 +0000

    generating k clusters. working on experiments now

--- kmodes.py
+++ kmodes.py
@@ -5,10 +5,12 @@ from mathtools import binVectorHammingDistance, isBinaryColVector, isBinaryColVe
 class Kmodes:
 	distanceFunction = None
 	trainingSet = None
+	indexClusters = []
 
-	def __init__(self, _distanceFunction, _trainingSet):
+	def __init__(self, _distanceFunction, _totalVectors):
 		self.distanceFunction = _distanceFunction
-		self.trainingSet = _trainingSet
+		self.totalVectors = _totalVectors
+		self.indexClusters = []
 
 	def modeOfBinVectorList(self, listOfVectors):
 		firstShape = np.shape(listOfVectors[0])
@@ -32,20 +34,101 @@ class Kmodes:
 			totalHammingDistances += binVectorHammingDistance(currentVector, targetVector)
 		return totalHammingDistances / len(listOfVectors)
 	
-"""		
+
+	def avgHammingDistancesForKClustersExperiment(self, k):
+		self.kClustersOfIndexes(k)
+		totalHammingDistances = 0
+		vectorClusters = []
+		#construct vectorClusters
+		for clusterIndex in range(len(self.indexClusters)):
+			vectorClusters.append([])
+			withinClusterHammingDistances = 0
+			for vectorIndex in self.indexClusters[clusterIndex]:
+				vectorClusters[clusterIndex].append(self.totalVectors[vectorIndex])
+			mode = self.modeOfBinVectorList(vectorClusters[clusterIndex])
+			for vector in vectorClusters[clusterIndex]:
+				withinClusterHammingDistances += binVectorHammingDistance(vector, mode)
+			print(f"for cluster {clusterIndex}, avg hamming distance is: {withinClusterHammingDistances / len(vectorClusters[clusterIndex])}")
+			totalHammingDistances += withinClusterHammingDistances
+		
+		return totalHammingDistances / len(self.totalVectors)
+
+
+					
+
+
+
+	def intListsHaveSameElements(self,l1, l2):
+		#now we know they have same lengths
+		if len(l1) != len(l2):
+			return False
+		for x in l1:
+		 	if x not in l2:
+		 		return False
+		return True
 	
-	def kClustersOfIndexes(k):
-		indexclusters = []
+	
+	def indexOfClosestCentroid(self, vector, centroidList):
+		closestCentroidIndex = 0
+		for i in range(len(centroidList)):
+				if binVectorHammingDistance(vector, centroidList[i]) < binVectorHammingDistance(vector, centroidList[closestCentroidIndex]):
+					closestCentroidIndex = i
+		return closestCentroidIndex
+
+	def kClustersOfIndexes(self,k):
+		indexClusters = []
 		centroids = []
-		trainingSetSet = np.copy(self.trainingSet)
+		totalVectorsSet = np.copy(self.totalVectors)
 		for cluster in range(k):
 			indexClusters.append([])
-			randIndex = randint(0, len(trainingSetSet)-1)
-			centroids.append(trainingSetSet[randIndex])
-			trainingSetSet.remove(randIndex)
+			randIndex = randint(0, len(totalVectorsSet)-1)
+			centroids.append(totalVectorsSet[randIndex])
+			totalVectorsSet = np.delete(totalVectorsSet, randIndex, 0)
 		
-		iterationsBeforeConvergence = 0
-		converged = True
+		#first assignments to random centroids
+		for vectorIndex in range(len(self.totalVectors)):
+			closestCentroidIndex = self.indexOfClosestCentroid(self.totalVectors[vectorIndex], centroids)
+			indexClusters[closestCentroidIndex].append(vectorIndex)
 
-		while not converged:a
-"""
+		
+		print(f"clusterIndexes after random assignment is\n {indexClusters}")
+		#now, find modes of clusters, those are new centroids.
+		#group vectors into new clusters using new centroids
+		#continue doing this until assignments to clusters are same as last iteration
+
+		iterationsBeforeConvergence = 0
+		converged = False
+		while not converged:
+			converged = True		
+			oldIndexClusters = np.copy(indexClusters)
+			#create vector clusters from indexClusters
+			vectorClusters = []
+			for i in range(k):
+				vectorClusters.append([])
+				for innerIndex in range(len(indexClusters[i])):
+					vectorClusters[i].append(self.totalVectors[indexClusters[i][innerIndex]])
+			#find mode for each cluster and set that as new centroid
+			for i in range(k):
+				if vectorClusters[i]:
+					centroids[i] = self.modeOfBinVectorList(vectorClusters[i])
+				else:
+					centroids[i] = np.zeros(np.shape(self.totalVectors[0]))
+
+			#assign to new clusters based on new centroids
+			indexClusters = []
+			for i in range(k):
+				indexClusters.append([])
+			for vectorIndex in range(len(self.totalVectors)):
+				closestCentroidIndex = self.indexOfClosestCentroid(self.totalVectors[vectorIndex], centroids)
+				indexClusters[closestCentroidIndex].append(vectorIndex)
+			iterationsBeforeConvergence +=1
+			
+			for i in range(k):
+				if not (self.intListsHaveSameElements(indexClusters[i], oldIndexClusters[i])):
+					converged = False
+					
+		
+		print(f"Kmodes.kClustersOfIndexes:: CONVERGED! took {iterationsBeforeConvergence} iterations")
+		self.indexClusters = indexClusters
+
+		return indexClusters
